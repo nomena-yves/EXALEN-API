22490551d766b52e694a5a4d2de2c593e251ae5c
nomena <hei.nomenjanahary@gmail.com>
2025-07-31 09:46:27 +0300

    l examen API

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
+from xmlrpc.client import DateTime
 from fastapi import FastAPI, Request, status
 from pydantic import BaseModel
 from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 from typing import List
 
+templates=Jinja2Templates(directory="HTML")
+
 app=FastAPI()
-class Tache(BaseModel):
-    id:int
-    titre:str
-    description:str
+@app.get("/ping")
+def ping_root():
+    return {"message":"pang"}
 
-tache_global:List[Tache]=[]
-
-@app.post("/tasks")
-def ajouter_root(tache_recu:List[Tache]):
-    for t in tache_recu:
-        tache_global.append(t)
-    return {"Message":"votre tache a été ajouter"}
+@app.get("/html", response_class=HTMLResponse)
+async def read_root():
+    return HTMLResponse("file.html")
 
 
-@app.get("/affiche")
-def afficher_root():
-    return tache_global
+class objet(BaseModel):
+    author:str
+    title:str
+    content:str
+    creation_dateTime:DateTime
 
+list_global:List[objet]=[]
 
-@app.delete("/delete")
-def supprimer_root(id:int):
-    for t in tache_global:
-        if t.id == id:
-            tache_global.remove(t)
-            return {"message": f"Tâche avec ID {id} supprimée."}
-    return {"message": f"Aucune tâche trouvée avec l'ID {id}."}
+@app.post("/posts")
+def post_root(object_add:List[objet]):
+    list_global.extend(object_add)
+    return list_global
+
+@app.get("/posts")
+def get_root():
+    return list_global
+
+@app.put("/posts")
+def put_root(title:str):
+    for index in list_global:
+        if index.title==title:
+            list_global.append(index)
+        list_global.extend(title)
+        return {"message":"la mise a jour de la liste à été faite"}
